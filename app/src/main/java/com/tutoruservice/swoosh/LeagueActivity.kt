43342d83aa3b7896fd022cf2de6fa3dfc7db7145
CJ Walker <cwalke39@gmail.com>
2019-02-18 19:26:25 -0700

    "Lecture 50"

--- app/src/main/java/com/tutoruservice/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/tutoruservice/swoosh/LeagueActivity.kt
@@ -2,8 +2,8 @@ package com.tutoruservice.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
-class LeagueActivity : AppCompatActivity() {
+// class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
